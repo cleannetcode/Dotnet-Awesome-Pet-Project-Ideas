7b888154b0f4b50df5ba352dfeec0c1a5856c2dc
Wisdou <dlisay430@gmail.com>
2023-04-05 23:18:24 +0300

    Add fizzbizz project

---
+++ ReadyTasks/CSharp/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static string GetFizzBuzzSeq(int numb)
+        {
+            if (numb % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (numb % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else if (numb % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return numb.ToString();
+            }
+        }
+
+        static IEnumerable<string> GetFizzBuzzSequence()
+        {
+            int st = 1;
+            while (true)
+            {
+                yield return GetFizzBuzzSeq(st);
+                st++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (var seqElem in GetFizzBuzzSequence().Take(10))
+            {
+                Console.WriteLine(seqElem);
+            }
+            Console.ReadKey();
+        }
+    }
+}
