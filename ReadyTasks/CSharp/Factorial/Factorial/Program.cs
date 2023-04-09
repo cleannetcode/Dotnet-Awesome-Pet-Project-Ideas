66c0333e5fa8976f17e206fc13c46dc8d6935332
Wisdou <51473681+Wisdou@users.noreply.github.com>
2023-04-09 20:12:44 +0300

    Add fibonacci, factorial and character count in string

---
+++ ReadyTasks/CSharp/Factorial/Factorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static BigInteger Factorial(int num) {
+            if (num < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(num));
+            }
+            
+            BigInteger result = BigInteger.One;
+            for (int i = 2; i <= num; i++)
+            {
+                result = result * i;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Write number: ");
+            int val = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Factorial of numnber {0} is equal to {1}", val, Factorial(val));
+        }
+    }
+}
---
+++ ReadyTasks/FSharp/Factorial/Factorial/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let factorial num =
+    if (num < 0) then
+        failwith "Number must be greater then 0"
+    else
+        Seq.init num (fun i -> bigint(i + 1)) |> Seq.fold (fun acc i -> acc * i) (1 |> bigint)
+
+[<EntryPoint>]
+let main argv =
+    Console.Write("Write number: ")
+    let numb = (Console.ReadLine >> int)()
+    printfn "Factorial of number %i is equal to %A" numb (factorial numb)
+    0 // return an integer exit code
