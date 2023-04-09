4145ccd0b8277f679cf39c824a650647266bea3f
Wisdou <51473681+Wisdou@users.noreply.github.com>
2023-04-09 21:20:44 +0300

    Add solution to first tasks

---
+++ ReadyTasks/CSharp/BMICalculator/BMICalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BMICalculator
+{
+    class Program
+    {
+        static double GetBMIValue(double weightInKg, double heightInM)
+        {
+            return weightInKg / Math.Pow(heightInM, 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Your weight in kilogramms: ");
+            double userWeight = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Your height in metres: ");
+            double userHeight = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Your BMI is: {0}", GetBMIValue(userWeight, userHeight));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReadyTasks/FSharp/FizzBuzz/FizzBuzz/Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+
+let getFizzBuzzElem (numb: int) =
+    if (numb % 15 = 0) then
+        "FizzBuzz"
+    elif (numb % 5 = 0) then
+        "Buzz"
+    elif (numb % 3 = 0) then
+        "Fizz"
+    else
+        numb.ToString()
+
+let getFizzBuzzElemByMatch (numb: int) =
+    match numb with
+    | a when a % 15 = 0 -> "FizzBuzz"
+    | a when a % 3 = 0 -> "Fizz"
+    | a when a % 5 = 0 -> "Buzz"
+    | a -> a.ToString()
+
+let getFizzBuzzSeq () =
+    Seq.initInfinite (fun x -> x + 1) |> Seq.map getFizzBuzzElemByMatch
+
+[<EntryPoint>]
+let main argv =
+    getFizzBuzzSeq () |> Seq.take 10 |> Seq.iter (printfn "%s")
+    0 // return an integer exit code
