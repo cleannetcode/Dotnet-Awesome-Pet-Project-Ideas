3bc60e8bd9cbac15ae84222caa003c3c765ebf79
Wisdou <51473681+Wisdou@users.noreply.github.com>
2023-04-11 23:10:54 +0300

    Add solution to number in radix base

---
+++ ReadyTasks/CSharp/CalculateDotAndCrossProductOfTwoVectors/CalculateDotAndCrossProductOfTwoVectors/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+// Unfinished: cross product
+namespace CalculateDotAndCrossProductOfTwoVectors
+{
+    class Vector
+    {
+        public double[] Coordinates {
+            get => this.coordinates;
+        }
+
+        private double[] coordinates;
+
+        public Vector(double[] _coordinates)
+        {
+            this.coordinates = _coordinates;
+        }
+
+        public int Dimensions
+        {
+            get => this.Coordinates.Length;
+        }
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            if (a.Dimensions == b.Dimensions)
+            {
+                return new Vector(a.Coordinates.Select((x, i) => a.Coordinates[i] + b.Coordinates[i]).ToArray());
+            }
+            else
+            {
+                throw new ArgumentException("Vector must have equal dimensions.");
+            }
+        }
+
+        public static Vector operator -(Vector a, Vector b)
+        {
+            if (a.Dimensions == b.Dimensions)
+            {
+                return new Vector(a.Coordinates.Select((x, i) => a.Coordinates[i] - b.Coordinates[i]).ToArray());
+            }
+            else
+            {
+                throw new ArgumentException("Vector must have equal dimensions.");
+            }
+        }
+
+        public static double ScalarProduct(Vector a, Vector b)
+        {
+            if (a.Dimensions == b.Dimensions)
+            {
+                return a.Coordinates.Select((x, i) => a.Coordinates[i] * b.Coordinates[i]).Sum();
+            }
+            else
+            {
+                throw new ArgumentException("Vector must have equal dimensions.");
+            }
+        }
+
+        public static Vector VectorProduct(Vector a, Vector b)
+        {
+            if (a.Dimensions == b.Dimensions)
+            {
+                return new Vector(a.Coordinates.Select((x, i) => a.Coordinates[i] - b.Coordinates[i]).ToArray());
+            }
+            else
+            {
+                throw new ArgumentException("Vector must have equal dimensions.");
+            }
+        }
+    }
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ReadyTasks/CSharp/RadixBaseConverter/RadixBaseConverter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RadixBaseConverter
+{
+    internal class Program
+    {
+        static string Alphabet = "0123456789ABCDEF";
+
+        static string ConvertToBase(int val, int radix)
+        {
+            Stack<char> result = new Stack<char>();
+            int absVal = Math.Abs(val);
+            while (absVal != 0)
+            {
+                result.Push(Program.Alphabet[absVal % radix]);
+                absVal /= radix;
+            }
+            if (val < 0)
+            {
+                result.Push('-');
+            }
+            return String.Concat(result);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Write number: ");
+            int val = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Write radix: ");
+            int radix = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Number {val} in radix {radix} is equal to {ConvertToBase(val, radix)}");
+            Console.ReadKey();
+        }
+    }
+}
