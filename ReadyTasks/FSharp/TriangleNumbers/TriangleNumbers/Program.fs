7eb50b392a95dd149c93105a44bb87927857abc4
Wisdou <dlisay430@gmail.com>
2023-04-13 23:24:47 +0300

    Add traingle numbers

---
+++ ReadyTasks/FSharp/TriangleNumbers/TriangleNumbers/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+// Define a function to construct a message to print
+
+let getTriangleNumbAtInd (ind: bigint) =
+    ind |> (fun x -> x * (x + bigint(1)) / bigint(2))
+
+let triangleSequence () =
+    Seq.initInfinite (fun x -> x + 1) |> Seq.map (bigint >> getTriangleNumbAtInd)
+
+[<EntryPoint>]
+let main argv =
+    triangleSequence() |> Seq.take 10 |> Seq.iter (printfn "%A")
+    0 // return an integer exit code
